5bd10bd56167a36011c97cf6c95cb7456efd93a3
jrior001 <jriordan001@gmail.com>
2015-06-25 22:49:46 -0400

    remove TV items

--- device.mk
+++ device.mk
@@ -126,12 +126,6 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.isUsbOtgEnabled=1
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
 
-# Include the Tv parts for use
-PRODUCT_PACKAGES += \
-    TvProvider \
-    TvSettings \
-    tv_input.default
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     bt_bcm4354
