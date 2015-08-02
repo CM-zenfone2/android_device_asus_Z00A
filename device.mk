add75963272fb68b01231122feaaf879e3b1f3d7
Christopher R. Palmer <crpalmer@gmail.com>
2015-08-02 06:23:04 -0400

    Revert "camera: Add a dummy wrapper HAL"
    This reverts commit 0c4dd2a29263826d7cc51cd63e4b030aa04d0d34.

--- device.mk
+++ device.mk
@@ -75,10 +75,6 @@ PRODUCT_PACKAGES += \
     charger \
     charger_res_images
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.mofd_v1 \
-
 # GPS
 PRODUCT_COPY_FILES += \
     device/asus/Z00A/configs/gps.conf:system/etc/gps.conf \
